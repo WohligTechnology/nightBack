e5398174f5810228d959d087694e4344fec8a7aa
vigkast <vigwohlig@gmail.com>
2016-04-07 16:52:34 +0530

    changes in schema and enquiry

--- api/services/Config.js
+++ api/services/Config.js
@@ -18,12 +18,15 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     homeContent: String,
     login: Schema.Types.Mixed,
-    blog: Schema.Types.Mixed
+    blog: Schema.Types.Mixed,
+    search: Schema.Types.Mixed,
+    gaid: String,
+    socialfeeds: Schema.Types.Mixed
 });
 module.exports = mongoose.model('Config', schema);
 
 var models = {
-    GlobalCallback: function(err, data, res) {
+    GlobalCallback: function (err, data, res) {
         if (err) {
             res.json({
                 error: err,
@@ -36,7 +39,7 @@ var models = {
             });
         }
     },
-    uploadFile: function(filename, callback) {
+    uploadFile: function (filename, callback) {
         var id = mongoose.Types.ObjectId();
         var extension = filename.split(".").pop();
         extension = extension.toLowerCase();
@@ -55,7 +58,7 @@ var models = {
                 filename: newFilename,
                 metadata: metaValue
             });
-            writestream2.on('finish', function() {
+            writestream2.on('finish', function () {
                 callback(null, {
                     name: newFilename
                 });
@@ -65,7 +68,7 @@ var models = {
         }
 
         if (extension == "png" || extension == "jpg" || extension == "gif") {
-            lwip.open(filename, extension, function(err, image) {
+            lwip.open(filename, extension, function (err, image) {
                 var upImage = {
                     width: image.width(),
                     height: image.height(),
@@ -74,13 +77,13 @@ var models = {
 
                 if (upImage.width > upImage.height) {
                     if (upImage.width > MaxImageSize) {
-                        image.resize(MaxImageSize, MaxImageSize / (upImage.width / upImage.height), function(err, image2) {
+                        image.resize(MaxImageSize, MaxImageSize / (upImage.width / upImage.height), function (err, image2) {
                             upImage = {
                                 width: image2.width(),
                                 height: image2.height(),
                                 ratio: image2.width() / image2.height()
                             };
-                            image2.writeFile(filename, function(err) {
+                            image2.writeFile(filename, function (err) {
                                 writer2(upImage);
                             });
                         });
@@ -89,13 +92,13 @@ var models = {
                     }
                 } else {
                     if (upImage.height > MaxImageSize) {
-                        image.resize((upImage.width / upImage.height) * MaxImageSize, MaxImageSize, function(err, image2) {
+                        image.resize((upImage.width / upImage.height) * MaxImageSize, MaxImageSize, function (err, image2) {
                             upImage = {
                                 width: image2.width(),
                                 height: image2.height(),
                                 ratio: image2.width() / image2.height()
                             };
-                            image2.writeFile(filename, function(err) {
+                            image2.writeFile(filename, function (err) {
                                 writer2(upImage);
                             });
                         });
@@ -108,18 +111,18 @@ var models = {
             imageStream.pipe(writestream);
         }
 
-        writestream.on('finish', function() {
+        writestream.on('finish', function () {
             callback(null, {
                 name: newFilename
             });
             fs.unlink(filename);
         });
     },
-    readUploaded: function(filename, width, height, style, res) {
+    readUploaded: function (filename, width, height, style, res) {
         var readstream = gfs.createReadStream({
             filename: filename
         });
-        readstream.on('error', function(err) {
+        readstream.on('error', function (err) {
             res.json({
                 value: false,
                 error: err
@@ -131,7 +134,7 @@ var models = {
                 filename: gridFSFilename,
                 metadata: metaValue
             });
-            writestream2.on('finish', function() {
+            writestream2.on('finish', function () {
                 fs.unlink(filename);
             });
             fs.createReadStream(filename).pipe(res);
@@ -142,7 +145,7 @@ var models = {
             var readstream2 = gfs.createReadStream({
                 filename: filename2
             });
-            readstream2.on('error', function(err) {
+            readstream2.on('error', function (err) {
                 res.json({
                     value: false,
                     error: err
@@ -173,7 +176,7 @@ var models = {
             var newNameExtire = newName + "." + extension;
             gfs.exist({
                 filename: newNameExtire
-            }, function(err, found) {
+            }, function (err, found) {
                 if (err) {
                     res.json({
                         value: false,
@@ -185,8 +188,8 @@ var models = {
                 } else {
                     var imageStream = fs.createWriteStream('./.tmp/uploads/' + filename);
                     readstream.pipe(imageStream);
-                    imageStream.on("finish", function() {
-                        lwip.open('./.tmp/uploads/' + filename, function(err, image) {
+                    imageStream.on("finish", function () {
+                        lwip.open('./.tmp/uploads/' + filename, function (err, image) {
                             ImageWidth = image.width();
                             ImageHeight = image.height();
                             var newWidth = 0;
@@ -223,8 +226,8 @@ var models = {
                                 newWidth = height * (ImageWidth / ImageHeight);
                                 newHeight = height;
                             }
-                            image.resize(parseInt(newWidth), parseInt(newHeight), function(err, image2) {
-                                image2.writeFile('./.tmp/uploads/' + filename, function(err) {
+                            image.resize(parseInt(newWidth), parseInt(newHeight), function (err, image2) {
+                                image2.writeFile('./.tmp/uploads/' + filename, function (err) {
                                     writer2('./.tmp/uploads/' + filename, newNameExtire, {
                                         width: newWidth,
                                         height: newHeight
@@ -241,14 +244,14 @@ var models = {
         }
         //error handling, e.g. file does not exist
     },
-    saveData: function(data, callback) {
+    saveData: function (data, callback) {
         var project = this(data);
         if (data._id) {
             this.findOneAndUpdate({
                 _id: data._id
             }, data, callback);
         } else {
-            project.save(function(err, data) {
+            project.save(function (err, data) {
                 if (err) {
                     callback(err, false);
                 } else {
@@ -257,10 +260,10 @@ var models = {
             });
         }
     },
-    deleteData: function(data, callback) {
+    deleteData: function (data, callback) {
         this.findOneAndRemove({
             _id: data._id
-        }, function(err, data) {
+        }, function (err, data) {
             if (err) {
                 callback(err, false);
             } else {
@@ -268,19 +271,19 @@ var models = {
             }
         });
     },
-    getAll: function(data, callback) {
+    getAll: function (data, callback) {
         this.find().exec(callback);
     },
-    getOne: function(data, callback) {
+    getOne: function (data, callback) {
         this.findOne({
             "_id": data._id
         }).exec(callback);
     },
-    getData: function(data, callback) {
+    getData: function (data, callback) {
         var newreturns = {};
         async.parallel([
-            function(callback) {
-                HomeSlider.getAll(data, function(err, data1) {
+            function (callback) {
+                HomeSlider.getAll(data, function (err, data1) {
                     if (err) {
                         newreturns.home = [];
                         callback(null, newreturns);
@@ -293,8 +296,8 @@ var models = {
                     }
                 });
             },
-            function(callback) {
-                Blog.getAll(data, function(err, data2) {
+            function (callback) {
+                Blog.getAll(data, function (err, data2) {
                     if (err) {
                         console.log(err);
                         newreturns.blog = [];
@@ -308,8 +311,8 @@ var models = {
                     }
                 });
             },
-            function(callback) {
-                Event.getAll(data, function(err, data3) {
+            function (callback) {
+                Event.getAll(data, function (err, data3) {
                     if (err) {
                         console.log(err);
                         newreturns.event = [];
@@ -323,8 +326,8 @@ var models = {
                     }
                 });
             },
-            function(callback) {
-                PhotoGallery.getAll(data, function(err, data4) {
+            function (callback) {
+                PhotoGallery.getAll(data, function (err, data4) {
                     if (err) {
                         console.log(err);
                         newreturns.photo = [];
@@ -338,8 +341,8 @@ var models = {
                     }
                 });
             },
-            function(callback) {
-                VideoGallery.getAll(data, function(err, data5) {
+            function (callback) {
+                VideoGallery.getAll(data, function (err, data5) {
                     if (err) {
                         console.log(err);
                         newreturns.video = [];
@@ -353,7 +356,7 @@ var models = {
                     }
                 });
             }
-        ], function(err, data6) {
+        ], function (err, data6) {
             if (err) {
                 console.log(err);
                 callback(err, null);
---
+++ api/services/Enquiry.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    name: String,
+    address: String,
+    subject: String,
+    email: String,
+    timestamp: Date,
+    comment: String
+});
+
+module.exports = mongoose.model('Enquiry', schema);
+var models = {
+    saveData: function (data, callback) {
+        var project = this(data);
+        if (data._id) {
+            this.findOneAndUpdate({
+                _id: data._id
+            }, data, callback);
+        } else {
+            project.save(function (err, data) {
+                if (err) {
+                    callback(err, false);
+                } else {
+                    callback(null, data);
+                }
+            });
+        }
+    },
+    deleteData: function (data, callback) {
+        this.findOneAndRemove({
+            _id: data._id
+        }, function (err, data) {
+
+            if (err) {
+                callback(err, false);
+            } else {
+                callback(null, data);
+            }
+        });
+    },
+    getAll: function (data, callback) {
+        this.find().exec(callback);
+    },
+    getOne: function (data, callback) {
+        this.findOne({
+            "_id": data._id
+        }).exec(callback);
+    }
+};
+
+module.exports = _.assign(module.exports, models);
